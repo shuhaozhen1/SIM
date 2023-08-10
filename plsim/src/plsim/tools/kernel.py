6a6045ec2ae94a1126591fd050cf5017b78afa8b
shuhaozhen1 <shuhaozhen@gmail.com>
2023-08-10 19:37:24 +0800

    tools

---
+++ plsim/src/plsim/tools/kernel.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def epanechnikov_kernel(x):
+    return np.where(np.abs(x) <= 1, 3/4 * (1 - x**2), 0)
+
+def uniform_kernel(x):
+    return np.where(np.abs(x) <= 1, 1/2, 0)
+
+def triangle_kernel(x):
+    return np.where(np.abs(x) <= 1, 1 - np.abs(x), 0)
+
+
