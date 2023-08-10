1a3ac0f3265c0a4823d10cbaf12ba05dd1f01cd7
shuhaozhen1 <shuhaozhen@gmail.com>
2023-08-10 21:14:22 +0800

    a

--- plsim/src/plsim/tools/kernel.py
+++ plsim/src/plsim/tools/kernel.py
@@ -9,3 +9,17 @@ def uniform_kernel(x):
 def triangle_kernel(x):
     return np.where(np.abs(x) <= 1, 1 - np.abs(x), 0)
 
+# Define a function that takes two arguments and returns their product
+def multiply(x, y):
+    return x * y
+
+# Create an array of numbers
+numbers = np.array([1, 2, 3, 4, 5])
+
+# Define the second argument
+y = 2
+
+# Apply the multiply function to each element of the numbers array
+# and provide the second argument y
+result = multiply(numbers, y)
+print(result) # [ 2  4  6  8 10]
