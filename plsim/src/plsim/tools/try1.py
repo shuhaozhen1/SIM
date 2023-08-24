63c3bceddd448cf4251e8fcc178719ad8f0b43c0
shuhaozhen1 <shuhaozhen@gmail.com>
2023-08-25 00:54:53 +0800

    a

--- plsim/src/plsim/tools/try1.py
+++ plsim/src/plsim/tools/try1.py
@@ -1,13 +1,22 @@
-from inference import bootstrap_inference
+
+from est import optimize_plsim_h, local_polynomial_regression
 
 import numpy as np
 
 # generate the data
 n = 200
-x= np.zeros((n,2))
 
-x[:,0] = np.random.uniform(0,1,size=n)
-x[:,1] = np.random.uniform(0,2,size=n)
+x12 = np.random.uniform(0,1,n)
+
+# Example usage:
+x= np.zeros((n,2))
+x[:,0] = np.random.uniform(0,0.6,n)
+x[:,1] =(x12 - 0.6*x[:,0])/0.8
+
+
+# x[:,0] = np.random.uniform(0,1,size=n)
+# x[:,1] = np.random.uniform(0,2,size=n)
+
 z = np.random.uniform(-1,1,size=(n,2))
 error = 0.1*np.random.randn(n)
 y = (0.6*x[:,0] + 0.8*x[:,1])**2 + 0.5*z[:,0] + 0.8*z[:,1] + error
@@ -17,24 +26,36 @@ data = {'x': x, 'z': z, 'y': y}
 kernel_type = 'epa'
 degree = 1
 
-x0 = np.linspace(0.2, 2 , 27)
-result = bootstrap_inference(data, kernel_type, degree, b_time=2000, 
-                              x0=x0, quantile=0.95)
+x0 = np.linspace(0, 1, 200)
+
+# result = optimize_plsim_h(data, kernel_type, degree, bandwidth=0.08)
+
+theta_hat, beta_hat, bandwidth = optimize_plsim_h(data, kernel_type, degree, bandwidth=0.11)
+
+x = data['x']
+z = data['z']
+y = data['y']
+
+y_transformed = y - theta_hat.T @ z.T
+x_transformed = beta_hat.T @ x.T
+data_transformed = np.column_stack((x_transformed,y_transformed))
+
+eta_hat = local_polynomial_regression(data=data_transformed, kernel_type=kernel_type, bandwidth=bandwidth, degree=degree, x0= x0)
 
 import matplotlib.pyplot as plt
 
+# # Assuming bs_result is a dictionary containing the results
+# scb_l = result['scb_l']
+# scb_u = result['scb_u']
 
-# Assuming bs_result is a dictionary containing the results
-scb_l = result['scb_l']
-scb_u = result['scb_u']
+# # Plot the band
+# plt.fill_between(x0, scb_l, scb_u, alpha=0.2)
 
-# Plot the band
-plt.fill_between(x0, scb_l, scb_u, alpha=0.2)
-
-# Plot the true function
-# Assuming true_function is a function that takes x0 as input and returns the true values
+# # Plot the true function
+# # Assuming true_function is a function that takes x0 as input and returns the true value
+plt.plot(x0, eta_hat[:,0])
 true_values = np.array(x0**2)
 plt.plot(x0, true_values)
 
-# Show the plot
+# # Show the plot
 plt.show()
